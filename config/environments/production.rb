ca549657dd9b3d51812e6e16b8f916ab6a2a62b1
IBRAHIMA <hadi.ibrahima8@gmail.com>
2022-04-12 15:16:18 +0100

    commit after 1st correction

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,4 +31,7 @@ Rails.application.configure do
   end
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.delivery_method = :letter_opener_web
+
 end
